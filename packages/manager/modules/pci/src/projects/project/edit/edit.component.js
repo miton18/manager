5b0fe2979a46731616814e4982c04cd2dc5dc595
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2021-01-21 14:27:39 +0100

    feat(pci/projects/project/edit): setup hds dependencies
    ref: MANAGER-6134
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/edit/edit.component.js
+++ packages/manager/modules/pci/src/projects/project/edit/edit.component.js
@@ -3,10 +3,14 @@ import template from './edit.html';
 
 export default {
   bindings: {
+    cart: '<',
+    checkout: '<',
     defaultProject: '<',
+    hds: '<',
     onUpdate: '<',
     project: '<',
     setDefault: '<',
+    summary: '<',
     unFavProject: '<',
   },
   controller,
--- packages/manager/modules/pci/src/projects/project/edit/edit.routing.js
+++ packages/manager/modules/pci/src/projects/project/edit/edit.routing.js
@@ -1,4 +1,6 @@
+import { Environment } from '@ovh-ux/manager-config';
 import { MESSAGES_CONTAINER_NAME } from './edit.constant';
+import { PCI_HDS_ADDON } from '../project.constants';
 
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('pci.projects.project.edit', {
@@ -9,6 +11,79 @@ export default /* @ngInject */ ($stateProvider) => {
     resolve: {
       breadcrumb: /* @ngInject */ ($translate) =>
         $translate.instant('pci_projects_project_parameters'),
+
+      cart: /* @ngInject */ (orderCart, hds) => {
+        if (hds.option && !hds.isCertifiedProject) {
+          return orderCart
+            .createNewCart(
+              Environment.getUser().ovhSubsidiary,
+              hds.option.planCode,
+            )
+            .then((cart) => orderCart.assignCart(cart.cartId).then(() => cart));
+        }
+
+        return null;
+      },
+
+      hds: /* @ngInject */ (
+        hdsProjectOption,
+        isCertifiedHdsProject,
+        isHdsAvailable,
+        isValidForHdsCertification,
+        isValidHdsSupportLevel,
+      ) => {
+        return {
+          isAvailable: isHdsAvailable,
+          isCertifiedProject: isCertifiedHdsProject,
+          isValidForCertification: isValidForHdsCertification,
+          isValidSupportLevel: isValidHdsSupportLevel,
+          option: hdsProjectOption,
+        };
+      },
+
+      hdsProjectOption: /* @ngInject */ ($http, projectId) =>
+        $http
+          .get(`/order/cartServiceOption/cloud/${projectId}`)
+          .then(({ data: options }) =>
+            options.find(({ planCode }) =>
+              planCode.match(`^${PCI_HDS_ADDON.planCodeScope}`),
+            ),
+          ),
+
+      isCertifiedHdsProject: /* @ngInject */ (
+        $http,
+        isValidForHdsCertification,
+        projectId,
+        OvhApiCloudProject,
+      ) => {
+        if (isValidForHdsCertification) {
+          return OvhApiCloudProject.ServiceInfos()
+            .v6()
+            .get({
+              serviceName: projectId,
+            })
+            .$promise.then((info) =>
+              $http
+                .get(`/services/${info.serviceId}/options`)
+                .then(
+                  ({ data: options }) =>
+                    options.find(
+                      ({ billing, resource }) =>
+                        billing.plan.code.match(
+                          `^${PCI_HDS_ADDON.planCodeScope}`,
+                        ) &&
+                        resource.product.name ===
+                          PCI_HDS_ADDON.certifiedProject,
+                    ) !== undefined,
+                ),
+            );
+        }
+        return false;
+      },
+
+      isValidForHdsCertification: /* @ngInject */ (hdsProjectOption) =>
+        hdsProjectOption !== undefined,
+
       onUpdate: /* @ngInject */ (
         $state,
         $timeout,
@@ -26,11 +101,31 @@ export default /* @ngInject */ ($stateProvider) => {
               ),
             ),
           ),
-      /* @ngInject */
-      setDefault: (PciProjectsService) => (projectId) =>
+
+      setDefault: /* @ngInject */ (PciProjectsService) => (projectId) =>
         PciProjectsService.setAsDefaultProject(projectId),
-      /* @ngInject */
-      unFavProject: (PciProjectsService) => () =>
+
+      summary: /* @ngInject */ (cart, project, orderCart, hds) => {
+        if (cart) {
+          const priceMode = hds.option.prices.find(({ capacities }) =>
+            capacities.includes('renew'),
+          );
+
+          return orderCart
+            .addOptionToCart(project.project_id, {
+              cartId: cart.cartId,
+              duration: priceMode.duration,
+              planCode: hds.option.planCode,
+              pricingMode: priceMode.pricingMode,
+              quantity: 1,
+            })
+            .then(() => orderCart.getSummary(cart.cartId));
+        }
+
+        return null;
+      },
+
+      unFavProject: /* @ngInject */ (PciProjectsService) => () =>
         PciProjectsService.removeDefaultProject(),
     },
   });
