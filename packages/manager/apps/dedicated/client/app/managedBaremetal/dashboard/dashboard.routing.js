05f933e8e4847d47001bd07eba8b033ce70d3d0d
varun257 <varun257@gmail.com>
2021-01-29 16:25:10 +0530

    feat(dedicated-cloud): add tracking to termination
    add at internet tracking to termination of services
    
    ref: MANAGER-5850
    
    Signed-off-by: varun257 <varun257@gmail.com>

--- packages/manager/apps/dedicated/client/app/managedBaremetal/dashboard/dashboard.routing.js
+++ packages/manager/apps/dedicated/client/app/managedBaremetal/dashboard/dashboard.routing.js
@@ -65,8 +65,13 @@ export default /* @ngInject */ ($stateProvider) => {
       onMlSubscribe: /* @ngInject */ ($state) => () =>
         $state.go('app.managedBaremetal.dashboard.ml-subscribe'),
 
-      onTerminate: /* @ngInject */ ($state) => () =>
-        $state.go('app.managedBaremetal.dashboard.terminate'),
+      onTerminate: /* @ngInject */ ($state, atInternet) => () => {
+        atInternet.trackClick({
+          name: 'dedicated::managedBaremetal::dashboard::terminate',
+          type: 'action',
+        });
+        $state.go('app.managedBaremetal.dashboard.terminate');
+      },
 
       onBasicOptionsUpgrade: /* @ngInject */ ($state) => (stateParams) =>
         $state.go(
